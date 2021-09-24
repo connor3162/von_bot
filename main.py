c49d343a57409bd2d3138e784828189ecf7b7337
connor3162 <60287940+connor3162@users.noreply.github.com>
2021-09-23 23:48:36 -0500

    changed the max_x variable to use the monitor width instead of a hard coded value.

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ import keyboard
 import time
 from lib.TemplateMatch import TemplateMatcher
 from lib.input import InputManager
+import ctypes
+user32 = ctypes.windll.user32
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     input_manager = InputManager()
     template_matcher = TemplateMatcher()
+    monitor_width = user32.GetSystemMetrics(0)
 
     while True:  # making a loop
         if keyboard.is_pressed('f4'):  # if key 'q' is pressed
@@ -23,13 +26,14 @@ if __name__ == '__main__':
                 while not template_matcher.ifTemplateExists("target", mask_name="target_mask"):
                     input_manager.tap_key("num0", 0.025)
                 print("found target reticle")
-                wh`ile not template_matcher.ifTemplateExists("purchase_this_plot_for"):
+                while not template_matcher.ifTemplateExists("purchase_this_plot_for"):
                     input_manager.tap_key("num0", 0.075)
                 print("found no button")
                 while not template_matcher.ifTemplateExists("hand", mask_name="hand_mask"):
                     input_manager.tap_key("num0", 0.025)
                 print("found hand")
-                while not template_matcher.ifTemplateExists("hand", mask_name="hand_mask", max_x=850, threshold=0.95):
+                while not template_matcher.ifTemplateExists("hand", mask_name="hand_mask", max_x=monitor_width * .4427
+                        , threshold=0.95):
                     input_manager.tap_key("num4", 0.04)
                 print("found hand and yes")
                 input_manager.tap_key("num0", 0.05)
