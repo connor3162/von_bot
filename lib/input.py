cf3fa00a287ea2a0a94d5684a71add4196eec9fe
connor3162 <60287940+connor3162@users.noreply.github.com>
2021-09-23 19:22:53 -0500

    refactored out the keyboard input functions out of main and into its own method called input.py located in lib.
    refactored out the keyboard input functions out of main and into its own method called input.py located in lib.

---
+++ lib/input.py
@@ -0,0 +1,27 @@
+from pykeyboard import PyKeyboard
+import time
+from random import uniform
+
+k = PyKeyboard()
+
+
+class InputManager:
+
+    vk = {
+        '5': '5', 'c': 'c', 'n': 'n', 'z': 'z', '3': '3', '1': '1', 'd': 'd', '0': '0', 'l': 'l', '8': 208,
+        'w': 'w', 'u': 'u', '4': '4', 'e': 'e', '[': '[', 'f': 'f', 'y': 'y', 'x': 'x', 'g': 'g', 'v': 'v',
+        'r': 'r', 'i': 'i', 'a': 'a', 'm': 'm', 'h': 'h', '.': '.', ',': ',', ']': ']', '/': '/', '6': '6',
+        '2': '2', 'b': 'b', 'k': 'k', '7': '7', 'q': 'q', "'": "'", '\\': 313, 'j': 'j', '`': '`', '9': '9',
+        'p': 'p', 'o': 'o', 't': 't', '-': '-', '=': '=', 's': 's', ';': ';', 'tab': k.tab_key, 'enter': k.enter_key,
+        'alt': k.alt_key,' ':' ',';':';','ctrl':k.control_key,'esc':k.escape_key,"f1": k.function_keys[1], "f2": k.function_keys[2],
+        "f3": k.function_keys[3], "f4": k.function_keys[4], "f5": k.function_keys[5],"f8": k.function_keys[8], "num0" : k.numpad_keys[0],
+        "num1": k.numpad_keys[1], "num2" : k.numpad_keys[2], "num3" : k.numpad_keys[3], "num4" : k.numpad_keys[4], "num5" : k.numpad_keys[5],
+        "num6": k.numpad_keys[6], "num7" : k.numpad_keys[7], "num8" : k.numpad_keys[8], "num9" : k.numpad_keys[9]
+    }
+
+    def tap_key(self, key, t=0.2):
+        print("pressed_key:" + key)
+        random_wait = uniform(-0.15, 0.15)
+        t = t + (t * random_wait)
+        k.tap_key(self.vk[key])
+        time.sleep(t)
--- main.py
+++ main.py
@@ -3,40 +3,15 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from pykeyboard import PyKeyboard
-import win32api,win32ui,win32con,win32gui
 import keyboard
 import time
-from random import uniform
 from lib.TemplateMatch import TemplateMatcher
+from lib.input import InputManager
 
-k=PyKeyboard()
-
-class VonBot():
-
-    vk = {
-        '5': '5', 'c': 'c', 'n': 'n', 'z': 'z', '3': '3', '1': '1', 'd': 'd', '0': '0', 'l': 'l', '8': 208,
-        'w': 'w', 'u': 'u', '4': '4', 'e': 'e', '[': '[', 'f': 'f', 'y': 'y', 'x': 'x', 'g': 'g', 'v': 'v',
-        'r': 'r', 'i': 'i', 'a': 'a', 'm': 'm', 'h': 'h', '.': '.', ',': ',', ']': ']', '/': '/', '6': '6',
-        '2': '2', 'b': 'b', 'k': 'k', '7': '7', 'q': 'q', "'": "'", '\\': 313, 'j': 'j', '`': '`', '9': '9',
-        'p': 'p', 'o': 'o', 't': 't', '-': '-', '=': '=', 's': 's', ';': ';', 'tab': k.tab_key, 'enter': k.enter_key,
-        'alt': k.alt_key,' ':' ',';':';','ctrl':k.control_key,'esc':k.escape_key,"f1": k.function_keys[1], "f2": k.function_keys[2],
-        "f3": k.function_keys[3], "f4": k.function_keys[4], "f5": k.function_keys[5],"f8": k.function_keys[8], "num0" : k.numpad_keys[0],
-        "num1": k.numpad_keys[1], "num2" : k.numpad_keys[2], "num3" : k.numpad_keys[3], "num4" : k.numpad_keys[4], "num5" : k.numpad_keys[5],
-        "num6": k.numpad_keys[6], "num7" : k.numpad_keys[7], "num8" : k.numpad_keys[8], "num9" : k.numpad_keys[9]
-    }
-
-    def tap_key(self, key, t=0.2):
-        print("pressed_key:" + key)
-        random_wait = uniform(-0.15, 0.15)
-        t = t + (t * random_wait)
-        k.tap_key(self.vk[key])
-        time.sleep(t)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    from main import VonBot
-    von_bot = VonBot()
+    input_manager = InputManager()
     template_matcher = TemplateMatcher()
 
     while True:  # making a loop
@@ -46,16 +21,16 @@ if __name__ == '__main__':
             while True:
                 print("starting new loop..")
                 while not template_matcher.ifTemplateExists("target", "target_mask"):
-                    von_bot.tap_key("num0", 0.05)
+                    input_manager.tap_key("num0", 0.05)
                 if keyboard.is_pressed('f5'):
                     exit()
-                von_bot.tap_key("num0", 0.1)
-                von_bot.tap_key("num0", 0.5)
-                von_bot.tap_key("num0", 0.1)
-                von_bot.tap_key("num0", 0.1)
-                von_bot.tap_key("num0", 0.1)
-                von_bot.tap_key("num4", 0.3)
-                von_bot.tap_key("num0", 0.5)
+                input_manager.tap_key("num0", 0.1)
+                input_manager.tap_key("num0", 0.5)
+                input_manager.tap_key("num0", 0.1)
+                input_manager.tap_key("num0", 0.1)
+                input_manager.tap_key("num0", 0.1)
+                input_manager.tap_key("num4", 0.3)
+                input_manager.tap_key("num0", 0.5)
                 print("ending loop!")
 
 0
